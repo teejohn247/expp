cd049703b44d0c96de68e59187b9e96aff551186
MOTY <50269513+MOTY12@users.noreply.github.com>
2023-09-21 16:07:47 +0100

    update server

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -119,7 +119,7 @@ export class MainServer {
 		});
 
 		const pubClient = createClient({
-			url: 'redis://127.0.0.1:6379'
+			url: 'process.env.REDIS_HOST'
 		});
 		const subClient = pubClient.duplicate();
 		log.info(`Redis started on port ${config.REDIS_HOST}`);
